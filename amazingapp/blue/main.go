743a017d54e9a37f09bcd4b1afba30ffa28e6e4b
satishr1 <satishramudri@gmail.com>
2025-03-13 17:15:58 +0530

    app code updated

--- amazingapp/blue/main.go
+++ amazingapp/blue/main.go
@@ -1,17 +1,48 @@
+// package main
+
+// import (
+//     "html/template"
+//     "log"
+//     "net/http"
+// )
+
+// func HomePage(w http.ResponseWriter, r *http.Request) {
+//     t, _ := template.ParseFiles("homepage.html")
+//     t.Execute(w, nil) 
+// }
+
+// func main() {
+//     http.HandleFunc("/", HomePage)
+//     log.Fatal(http.ListenAndServe(":8080", nil))
+// }
+
+
 package main
 
 import (
-    "html/template"
-    "log"
-    "net/http"
+        "html/template"
+        "log"
+        "net/http"
 )
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-    t, _ := template.ParseFiles("homepage.html")
-    t.Execute(w, nil) 
+        tmpl, err := template.ParseFiles("homepage.html") // Replace "homepage.html" with your template file
+        if err != nil {
+                log.Printf("Error parsing template: %v", err)
+                http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+                return
+        }
+
+        err = tmpl.Execute(w, nil) // Execute the loaded template
+        if err != nil {
+                log.Printf("Error executing template: %v", err)
+                http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+                return
+        }
 }
 
 func main() {
-    http.HandleFunc("/", HomePage)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+        http.HandleFunc("/", HomePage)
+        log.Println("Server listening on port 8080")
+        log.Fatal(http.ListenAndServe(":8080", nil))
 }
